ce729c96c306dcb1bb5b03a72cdf5fa078c083e0
Nico Arato <nico.arato@gmail.com>
2020-05-02 02:17:08 -0300

    Dom Scripting.

---
+++ DOM-scripting/universidad/js/app.js
@@ -0,0 +1,28 @@
+// Eliminar de Local Storage
+localStorage.clear();
+
+let elemento;
+elemento = document;
+elemento = document.all;
+elemento = document.all[10];
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+elemento = document.URL;
+elemento = document.characterSet;
+elemento = document.forms;
+elemento = document.forms[0];
+elemento = document.forms[0].id;
+elemento = document.forms[0].className;
+elemento = document.forms[0].classList;
+elemento = document.images;
+elemento = document.images[2];
+elemento = document.images[2].src;
+elemento = document.images[2].getAttribute('src');
+
+
+//scripts
+
+elemento = document.scripts;
+
+console.log(elemento);
