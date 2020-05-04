c4c399e110f1bf8dbbd423aad20906dd78594f97
Nico Arato <nico.arato@gmail.com>
2020-05-03 21:42:04 -0300

    Delegation.

---
+++ DOM-scripting/universidad/js/app15.js
@@ -0,0 +1,15 @@
+document.body.addEventListener('click', eliminarElemento);
+
+function eliminarElemento(e) {
+    e.preventDefault();
+    console.log('click');
+    console.log(e.target.classList);
+
+    if (e.target.classList.contains('borrar-curso')) {
+        console.log(e.target.parentElement.parentElement.remove());
+    }
+
+    if (e.target.classList.contains('agregar-carrito')) {
+        console.log('Curso agregado')
+    }
+}
