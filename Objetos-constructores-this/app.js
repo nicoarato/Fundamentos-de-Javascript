cea2301d5ed7c046158765be2a2f6f29c4e4ed39
Nico Arato <nico.arato@gmail.com>
2020-05-25 21:12:00 -0300

    Objetos y constructores.

--- Objetos-constructores-this/app.js
+++ Objetos-constructores-this/app.js
@@ -12,3 +12,29 @@ if (typeof nombre1 == typeof nombre2) {
 
     console.log('No son iguales.')
 }
+
+//boolean
+const boolean1 = true;
+const boolean2 = new Boolean(true);
+
+
+//funciones
+const f1 = function(a, b) {
+    return a + b;
+}
+
+const f2 = new Function('a', 'b', 'return a + b');
+
+// objetos
+const persona1 = {
+    nombre: 'Juan'
+}
+
+const persona2 = new Object({ nombre: 'Juan' })
+
+// arreglos
+const arreglo1 = [1, 2, 3, 4];
+const arreglo2 = new Array(1, 2, 3, 4);
+
+console.log(arreglo1);
+console.log(arreglo2);
