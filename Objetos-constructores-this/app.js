c9b895706f1f8a4d566a66b4229feb448d689fc2
Nico Arato <nico.arato@gmail.com>
2020-05-26 01:47:27 -0300

    Object create.

--- Objetos-constructores-this/app.js
+++ Objetos-constructores-this/app.js
@@ -1,32 +1,14 @@
-function Cliente(nombre, saldo) {
-    this.nombre = nombre;
-    this.saldo = saldo;
+const Cliente = {
+        imprimirSaldo: function() {
+            return `Hola ${this.nombre} tu saldo es ${this.saldo}`
+        },
+        retirarSaldo: function(retiro) {
+            return this.saldo -= retiro;
+        }
+    }
+    // Crear el objeto
+const maru = Object.create(Cliente);
+maru.nombre = 'Maru';
+maru.saldo = 100;
 
-
-}
-
-
-// prototipo que imprime saldo y nombre
-
-Cliente.prototype.nombreClienteSaldo = function() {
-    return `Nombre: ${this.nombre} - Saldo: ${this.saldo} `;
-}
-
-const cliente1 = new Cliente('Pedro', 1400);
-console.log(cliente1.nombreClienteSaldo());
-
-
-
-function Empresa(nombre, saldo, telefono, tipo) {
-    Cliente.call(this, nombre, saldo); //Herencia de prototypes.
-    this.telefono = telefono;
-    this.tipo = tipo;
-}
-
-// heredar prototypes
-Empresa.prototype = Object.create(Cliente.prototype);
-
-const empresa = new Empresa('SASASA', 1000000, 123456789, 'Educacion');
-
-console.log(empresa);
-console.log(empresa.nombreClienteSaldo());
+console.log(maru.imprimirSaldo())
