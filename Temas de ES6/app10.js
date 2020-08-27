1a1c2e7dda27abd64177c6ddb898adb14e1ecab9
Nico Arato <nico.arato@gmail.com>
2020-08-26 22:08:21 -0300

    Cual está bien y porque?.
    Hoisting.  2 contextos de ejecución en js. La segunda opción tiene una sintaxis como si fuera una variable, la estas usando antes de la inicialización.

---
+++ Temas de ES6/app10.js
@@ -0,0 +1,8 @@
+obtenerNombre('nico')
+
+function obtenerNombre(nombre) {
+    console.log('su nombre es ' + nombre)
+}
+
+obtenerApellido('Arato')
+const obtenerApellido = (ape) => console.log('Su apellido es : ' + ape)
